50240e4b0c908354af8167195323ab5284e4a9fa
MiguelD-ciber <83778713+MiguelD-ciber@users.noreply.github.com>
2024-03-04 16:06:44 -0300

    Update Donut.py

--- Donut.py
+++ Donut.py
@@ -1,104 +1,255 @@
-import math
+import os
+
+from math import cos, sin
+
+import pygame
+
+import colorsys
 
 
-def render_frame(A, B):
 
-    # Precompute sines and cosines of A and B
-    cosA = math.cos(A)
-    sinA = math.sin(A)
-    cosB = math.cos(B)
-    sinB = math.sin(B)
+WHITE = (255, 255, 255)
 
-    char_output = []
-    zbuffer = []
+BLACK = (0, 0, 0)
 
-    for i in range(screen_height + 1):
-        char_output.append([' '] * (screen_width + 0))
-        zbuffer.append([0] * (screen_width + 0))
+hue = 0
 
-    # theta goes around the cross-sectional circle of a torus
-    theta = 0
-    while (theta < 2* math.pi):
-        theta += theta_spacing
 
-        # Precompute sines and cosines of theta
-        costheta = math.cos(theta)
-        sintheta = math.sin(theta)
 
-        # phi goes around the center of revolution of a torus
-        phi = 0
-        while (phi < 2*math.pi):
-            phi += phi_spacing
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-            # Precompute sines and cosines of phi
-            cosphi = math.cos(phi)
-            sinphi = math.sin(phi)
+RES = WIDTH, HEIGHT = 800, 800
 
-            # the x,y coordinate of the circle,
-            # before revolving (factored out of the above equations)
-            circlex = R2 + R1*costheta
-            circley = R1*sintheta
+FPS = 60
 
-            # final 3D (x,y,z) coordinate after rotations, directly from our math above
-            x = circlex*(cosB*cosphi + sinA*sinB*sinphi) - circley*cosA*sinB
-            y = circlex*(sinB*cosphi - sinA*cosB*sinphi) + circley*cosA*cosB
-            z = K2 + cosA*circlex*sinphi + circley*sinA
-            ooz = 1/z
 
-            # x and y projection. y is negated here, because y goes up in
-            # 3D space but down on 2D displays.
-            xp = int(screen_width/2 + K1*ooz*x)
-            yp = int(screen_height/2 - K1*ooz*y)
 
-            # Calculate luminance
-            L = cosphi*costheta*sinB - cosA*costheta*sinphi - sinA*sintheta + cosB*(cosA*sintheta - costheta*sinA*sinphi)
+pixel_width = 20
 
-            # L ranges from -sqrt(2) to +sqrt(2).  If it's < 0, the surface is
-            # pointing away from us, so we won't bother trying to plot it.
-            if L > 0:
-                # Test against the z-buffer. Larger 1/z means the pixel is closer to
-                # the viewer than what's already plotted.
-                if ooz > zbuffer[xp][yp]:
-                    zbuffer[xp][yp] = ooz
-                    luminance_index = L*8   # this brings L into the range 0..11 (8*sqrt(2) = 11.3)
+pixel_height = 20
 
-                    # Now we lookup the character corresponding
-                    # to the luminance and plot it in our output
-                    char_output[xp][yp] = '.,-~:;=!*#$@'[int(luminance_index)]
 
-    # Now, dump char_output to the screen.
-    # Bring cursor to "home" location, in just about any currently-used terminal emulation mode
-    print('\x1b[H')
+
+x_pixel = 0
+
+y_pixel = 0
+
+
+
+screen_width = WIDTH // pixel_width
+
+screen_height = HEIGHT // pixel_height
+
+screen_size = screen_width * screen_height
+
+
+
+A, B = 0, 0
+
+
+
+theta_spacing = 10
+
+phi_spacing = 3
+
+
+
+chars = ".,-~:;=!*#$@"
+
+
+
+R1 = 10
+
+R2 = 20
+
+K2 = 200
+
+K1 = screen_height * K2 * 3 / (8 * (R1 + R2))
+
+
+
+pygame.init()
+
+
+
+screen = pygame.display.set_mode(RES)
+
+clock = pygame.time.Clock()
+
+font = pygame.font.SysFont('Arial', 20, bold=True)
+
+
+
+
+
+def hsv2rgb(h, s, v):
+
+    return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h, s, v))
+
+
+
+
+
+def text_display(char, x, y):
+
+    text = font.render(str(char), True, hsv2rgb(hue, 1, 1))
+
+    text_rect = text.get_rect(center=(x, y))
+
+    screen.blit(text, text_rect)
+
+
+
+
+
+k = 0
+
+
+
+paused = False
+
+running = True
+
+while running:
+
+    clock.tick(FPS)
+
+    pygame.display.set_caption("FPS: {:.2f}".format(clock.get_fps()))
+
+    screen.fill(BLACK)
+
+
+
+    output = [' '] * screen_size
+
+    zbuffer = [0] * screen_size
+
+
+
+    for theta in range(0, 628, theta_spacing):  # theta goes around the cross-sectional circle of a torus, from 0 to 2pi
+
+        for phi in range(0, 628, phi_spacing):  # phi goes around the center of revolution of a torus, from 0 to 2pi
+
+
+
+            cosA = cos(A)
+
+            sinA = sin(A)
+
+            cosB = cos(B)
+
+            sinB = sin(B)
+
+
+
+            costheta = cos(theta)
+
+            sintheta = sin(theta)
+
+            cosphi = cos(phi)
+
+            sinphi = sin(phi)
+
+
+
+            # x, y coordinates before revolving
+
+            circlex = R2 + R1 * costheta
+
+            circley = R1 * sintheta
+
+
+
+            # 3D (x, y, z) coordinates after rotation
+
+            x = circlex * (cosB * cosphi + sinA * sinB * sinphi) - circley * cosA * sinB
+
+            y = circlex * (sinB * cosphi - sinA * cosB * sinphi) + circley * cosA * cosB
+
+            z = K2 + cosA * circlex * sinphi + circley * sinA
+
+            ooz = 1 / z  # one over z
+
+
+
+            # x, y projection
+
+            xp = int(screen_width / 2 + K1 * ooz * x)
+
+            yp = int(screen_height / 2 - K1 * ooz * y)
+
+
+
+            position = xp + screen_width * yp
+
+
+
+            # luminance (L ranges from -sqrt(2) to sqrt(2))
+
+            L = cosphi * costheta * sinB - cosA * costheta * sinphi - sinA * sintheta + cosB * (
+
+                        cosA * sintheta - costheta * sinA * sinphi)
+
+
+
+            if ooz > zbuffer[position]:
+
+                zbuffer[position] = ooz  # larger ooz means the pixel is closer to the viewer than what's already plotted
+
+                luminance_index = int(L * 8)  # we multiply by 8 to get luminance_index range 0..11 (8 * sqrt(2) = 11)
+
+                output[position] = chars[luminance_index if luminance_index > 0 else 0]
+
+
+
     for i in range(screen_height):
+
+        y_pixel += pixel_height
+
         for j in range(screen_width):
-            print(char_output[i][j], end='')
-        print()
+
+            x_pixel += pixel_width
+
+            text_display(output[k], x_pixel, y_pixel)
+
+            k += 1
+
+        x_pixel = 0
+
+    y_pixel = 0
+
+    k = 0
 
 
-theta_spacing = 0.07
-phi_spacing = 0.02
 
-R1 = 1
-R2 = 2
-K2 = 5
+    A += 0.15
 
-# Calculate K1 based on screen size: the maximum x-distance occurs roughly at
-# the edge of the torus, which is at x=R1+R2, z=0.  we want that to be
-# displaced 3/8ths of the width of the screen, which is 3/4th of the way from
-# the center to the side of the screen.
-# screen_width*3/8 = K1*(R1+R2)/(K2+0)
-# screen_width*K2*3/(8*(R1+R2)) = K1
+    B += 0.035
 
-screen_width = 35
-screen_height = 35
 
-K1 = screen_width*K2*3/(8*(R1+R2))
 
-print('\x1b[2J')
-A = 1.0
-B = 1.0
+    hue += 0.005
 
-for i in range(250):
-    render_frame(A, B)
-    A += 0.08
-    B += 0.03
+
+
+    if not paused:
+
+        pygame.display.update()
+
+
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_ESCAPE:
+
+                running = False
+
+            if event.key == pygame.K_SPACE:
+
+                paused = not paused
